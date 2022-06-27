225e2fa3ebfa84efd34a64c4489d561df1a40cd3
Chris White <chrisw-07@hotmail.co.uk>
2022-06-27 12:51:29 +0000

    Update before branch switch

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,9 @@
-class ContactMaier < ActionMailer::Base
-  default to: 'chrisw@hotmail.co.uk'
+class ContactMailer < ActionMailer::Base
+  default to: 'chrisw-07@hotmail.co.uk'
   def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
-    
     mail(from: email, subject: 'Contact Form Message')
   end
 end
